baeaa974251567b70eae8721be748adcc233d713
Roberto Cervantes <robece@protonmail.com>
2021-12-15 14:00:21 -0800

    Update HttpTrigger1.cs

--- source/HttpTrigger1.cs
+++ source/HttpTrigger1.cs
@@ -11,23 +11,6 @@ using Azure.Messaging.EventGrid;
 using Azure;
 using Azure.Identity;
 
-/*
-Instructions to follow:
-
-There are multiple scenarios to authenticate using Azure Active Directory in this scenario we are using an Application with a client secret.
-Make sure you already have created the Application in Azure Active Directory and the client secret generated (you will need to provide this information in the code).
-In the Event Grid topic add a "role assignment" using system assigned feature in Identity.
-The role assignment will provide the grants to the Application to communicate with the topic using the role: EventGrid Data Sender, otherwise the code will fail.
-
-References: 
-
-Azure Event Grid client library for .NET
-https://github.com/Azure/azure-sdk-for-net/tree/main/sdk/eventgrid/Azure.Messaging.EventGrid#authenticate-using-azure-active-directory
-
-Azure Identity client library for .NET
-https://github.com/Azure/azure-sdk-for-net/blob/main/sdk/identity/Azure.Identity/README.md
-*/
-
 namespace Company.Function
 {
     public static class HttpTrigger1
